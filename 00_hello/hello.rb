ca4920db849312dd938b166bbb8f7c3b0954ca5a
Louis Aurick <louisaurick@ntymail.com>
2018-01-16 14:29:00 +0100

    Ajout exercices 1 et 2

--- 00_hello/hello.rb
+++ 00_hello/hello.rb
@@ -1,1 +1,8 @@
 #write your code here
+def hello
+	"Hello!"
+end
+
+def greet(nom)
+	"Hello, #{nom}!"
+end
--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,14 @@
 #write your code here
+	def ftoc(fahrenheit)
+
+		((fahrenheit - 32) / (9.0 / 5.0)).round(2)
+	end
+
+	def ctof(celsius)
+
+		((celsius) * (9.0 / 5.0) + 32).round(2)
+
+	end
+
+	puts ftoc(32)
+	puts ctof(37)
