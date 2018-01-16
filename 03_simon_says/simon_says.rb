cc7db888e589322651c558cfdf5018c9a497eabf
Louis Aurick <louisaurick@ntymail.com>
2018-01-16 18:41:38 +0100

    upgarde exo3

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,30 @@
 #write your code here
+
+def echo(word)
+	"#{word}"
+end
+
+def shout(word)
+	"#{word}".upcase
+end
+
+def repeat(words, n=2)
+	result = words
+	(n-1).times do (result += " " + words)
+	end
+	result
+end
+
+def start_of_word(word, n)
+	array = word.chars
+	array[0]
+end
+
+def first_word
+	
+end
+
+def titleize
+	
+end
+
