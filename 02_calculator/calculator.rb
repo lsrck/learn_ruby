e2431c03b5dbc365399bb4c8193f9dabbcf0918c
Louis Aurick <louisaurick@ntymail.com>
2018-01-16 17:17:33 +0100

    Ajout exo2 + bonus

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -12,3 +12,29 @@ def sum(array)
 	array.each {|n| sum += n}
 	return sum
 end
+
+def multiply(*numbers)
+	multi = 1
+	numbers.each { |n| multi *= n }
+	return multi
+end
+
+def power(number1p,number2p)
+	number1p ** number2p
+end
+
+def factorial(facto)
+	#permet de retourner 1 si facto = 0
+	if facto == 0 
+		return 1
+	else
+	result = 1
+	i = 1
+	1.upto(facto) do
+	puts result = result * i
+	puts i = i + 1
+		end
+	end
+end
+
+factorial(10)
