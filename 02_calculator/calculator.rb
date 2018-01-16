e0602c48eda55e93754fed9aaa252b14bcb38eac
Louis Aurick <louisaurick@ntymail.com>
2018-01-16 15:05:51 +0100

    Ajout exercice02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,14 @@
 #write your code here
+def add(number1a, number2a)
+	number1a + number2a
+end
+
+def subtract(number1s, number2s)
+	number1s - number2s
+end
+
+def sum(array)
+	sum = 0
+	array.each {|n| sum += n}
+	return sum
+end
